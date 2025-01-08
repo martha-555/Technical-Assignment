96af8f6eecd1a6fa5efa40d7a643569a2f65c48d
martha-555 <112080638+martha-555@users.noreply.github.com>
2025-01-09 01:09:59 +0200

    moved routing to a separate module

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,10 @@
 
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import SelectedRecipes from "./pages/SelectedRecipes/SelectedRecipes";
-import ListOfAllRecipes from "./pages/ListOfAllRecipes/ListOfAllRecipes";
 import { QueryClient, QueryClientProvider } from "react-query";
-import FoundRecipes from "./pages/FoundRecipes/FoundRecipes";
-import CardProvider from "./context/CardProvider";
-import CardDetails from "./pages/CardDetails/CardDetails";
+import { routes } from "./routes/routes";
 
-const routers = createBrowserRouter([
-  {
-    path: "*",
-    element: (
-      <CardProvider>
-        <ListOfAllRecipes />
-      </CardProvider>
-    ),
-  },
-  {
-    path: "/all_recipes",
-    element: (
-      <CardProvider>
-        <ListOfAllRecipes />
-      </CardProvider>
-    ),
-  },
-  {
-    path: "/selected",
-    element: (
-      <CardProvider>
-        <SelectedRecipes />
-      </CardProvider>
-    ),
-  },
-  {
-    path: "/found",
-    element: (
-      <CardProvider>
-        <FoundRecipes />
-      </CardProvider>
-    ),
-  },
-  {
-    path: "/details",
-    element: (
-      <CardProvider>
-        <CardDetails />
-      </CardProvider>
-    ),
-  },
-]);
+const routers = createBrowserRouter(routes);
 function App() {
   const queryClient = new QueryClient();
 
---
+++ src/routes/routes.tsx
@@ -0,0 +1,50 @@
+/** @format */
+
+import CardProvider from "../context/CardProvider";
+import CardDetails from "../pages/CardDetails/CardDetails";
+import FoundRecipes from "../pages/FoundRecipes/FoundRecipes";
+import ListOfAllRecipes from "../pages/ListOfAllRecipes/ListOfAllRecipes";
+import SelectedRecipes from "../pages/SelectedRecipes/SelectedRecipes";
+
+export const routes = [
+  {
+    path: "*",
+    element: (
+      <CardProvider>
+        <ListOfAllRecipes />
+      </CardProvider>
+    ),
+  },
+  {
+    path: "/all_recipes",
+    element: (
+      <CardProvider>
+        <ListOfAllRecipes />
+      </CardProvider>
+    ),
+  },
+  {
+    path: "/selected",
+    element: (
+      <CardProvider>
+        <SelectedRecipes />
+      </CardProvider>
+    ),
+  },
+  {
+    path: "/found",
+    element: (
+      <CardProvider>
+        <FoundRecipes />
+      </CardProvider>
+    ),
+  },
+  {
+    path: "/details",
+    element: (
+      <CardProvider>
+        <CardDetails />
+      </CardProvider>
+    ),
+  },
+];
